7df71efc7d07dca7e8d30c91c8020c4671e8b299
Seobok <100834254+Seobok@users.noreply.github.com>
2026-01-09 14:09:59 +0900

    Add tutorial dungeon layout mode

--- Assets/Scripts/Workflows/DungeonSceneWorkflow.cs
+++ Assets/Scripts/Workflows/DungeonSceneWorkflow.cs
@@ -19,6 +19,8 @@ namespace Workflows
         private UnitManager _unitManager;
         private EffectPool _effectPool;
 
+        [SerializeField] private bool isTutorial;
+
 
         [Inject]
         public void Construct(Dungeon dungeon, UnitManager unitManager, EffectPool effectPool)
@@ -31,34 +33,45 @@ namespace Workflows
         private void Start()
         {
             _effectPool.Init();
-            _dungeon.ActivateDungeon(7);
+            DungeonGenerationMode generationMode = isTutorial
+                ? DungeonGenerationMode.TutorialPreset
+                : DungeonGenerationMode.RandomExpand;
+            _dungeon.ActivateDungeon(7, generationMode);
 
             Knight knight = _unitManager.SpawnKnight(0, 0);
             knight.PlayerController.NextTurn += NextTurn;
 
-            // 적 15마리 스폰
-            for (int i = 0; i < 15; i++)
+            if (isTutorial)
             {
-                Coord spawnPosition = _dungeon.GetRandomEmptyTileFromRooms(100);
-                
-                if (spawnPosition != Coord.Zero) // 적절한 위치를 찾은 경우
+                _unitManager.SpawnEnemy(typeof(Slime), 2, 0);
+                _unitManager.SpawnEnemy(typeof(Bat), 5, 0);
+            }
+            else
+            {
+                // 적 15마리 스폰
+                for (int i = 0; i < 15; i++)
                 {
-                    int random = Random.Range(0, 2);
-                    switch (random)
-                    {
-                        case 0:
-                            _unitManager.SpawnEnemy(typeof(Bat), spawnPosition.X, spawnPosition.Y);
-                            break;
-                        case 1:
-                            _unitManager.SpawnEnemy(typeof(Slime), spawnPosition.X, spawnPosition.Y);
-                            break;
-                    }
+                    Coord spawnPosition = _dungeon.GetRandomEmptyTileFromRooms(100);
                     
-                }
-                else
-                {
-                    Debug.LogWarning("더 이상 적을 스폰할 수 있는 빈 공간이 없습니다.");
-                    break;
+                    if (spawnPosition != Coord.Zero) // 적절한 위치를 찾은 경우
+                    {
+                        int random = Random.Range(0, 2);
+                        switch (random)
+                        {
+                            case 0:
+                                _unitManager.SpawnEnemy(typeof(Bat), spawnPosition.X, spawnPosition.Y);
+                                break;
+                            case 1:
+                                _unitManager.SpawnEnemy(typeof(Slime), spawnPosition.X, spawnPosition.Y);
+                                break;
+                        }
+                        
+                    }
+                    else
+                    {
+                        Debug.LogWarning("더 이상 적을 스폰할 수 있는 빈 공간이 없습니다.");
+                        break;
+                    }
                 }
             }
         }
